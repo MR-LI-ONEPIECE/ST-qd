515dc868fb251232e274468d77de0688e932db4c
lizekun <div_box@163.com>
2022-03-22 13:40:58 +0800

    asdf

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get("/api/wx_openid", async (req, res) => {
 app.post("/api/userInfo", async (req, res) => {
   const result = req.body;
   try {
-    const userInfo = await UserInfo.upsert({ openId: result.openId }, result)
+    const userInfo = await UserInfo.upsert(result)
     console.log(userInfo)
     res.send({
       code: 0,
